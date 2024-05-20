01c5d0391fb60272541302b5ef2e7efd1282a37c
Joseph Mearman <joseph@mearman.co.uk>
2024-05-20 03:43:19 +0100

    Update apt install commands in 02-run.sh

--- meshtastic-stage/meshtastic/02-run.sh
+++ meshtastic-stage/meshtastic/02-run.sh
@@ -17,7 +17,8 @@ echo "ASSET_URL $ASSET_URL"
 FILE_NAME=meshtasticd_arm64.deb
 
 curl -L -o $FILE_NAME $ASSET_URL
-apt install -y -f libyaml-cpp-dev libulfius-dev
+apt install -y -f libyaml-cpp0.7:arm64
+apt install -y -f libulfius2.7:arm64
 apt install -y -f ./$FILE_NAME
 
 apt clean
