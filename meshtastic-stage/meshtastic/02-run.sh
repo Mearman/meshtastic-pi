e515f9957c56af4d65876ce9d5b273bdc3e37016
Joseph Mearman <joseph@mearman.co.uk>
2024-05-20 05:33:14 +0100

    Update apt install commands in 02-run.sh

--- meshtastic-stage/meshtastic/02-run.sh
+++ meshtastic-stage/meshtastic/02-run.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 set -e
 
-apt update && apt install -y jq
+VERSION=${1:-prerelease}
 
-LATEST_RELEASE=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/meshtastic/firmware/releases/latest | jq -r '.')
-echo "LATEST_RELEASE: $LATEST_RELEASE"
+if [ "$VERSION" == "latest" ]; then
+    echo "Fetching latest release"
+elif [ "$VERSION" == "prerelease" ]; then
+    echo "Fetching latest prerelease"
+else
+    echo "Fetching release $VERSION"
+fi
 
-BINARY_PACKGE_ASSET=$(echo $LATEST_RELEASE | jq -r '.assets[] | select(.content_type == "application/vnd.debian.binary-package")')
+RELEASES=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/meshtastic/firmware/releases)
+
+if [ -z "$RELEASES" ]; then
+    echo "Failed to fetch releases"
+    exit 1
+fi
+
+if [ "$VERSION" == "latest" ]; then
+    RELEASE=$(curl -s -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/meshtastic/firmware/releases/latest)
+elif [ "$VERSION" == "prerelease" ]; then
+    RELEASE=$(echo $RELEASES | jq -r '.[0] | select(.prerelease == true and .draft == false) | .')
+else
+    RELEASE=$(echo $RELEASES | jq -r '.[0] | select(.tag_name == "'$VERSION'")')
+fi
+
+if [ -z "$RELEASE" ]; then
+    echo "Failed to fetch release"
+    exit 1
+else
+    echo "RELEASE: $RELEASE"
+fi
+
+BINARY_PACKGE_ASSET=$(echo $RELEASE | jq -r '.assets[] | select(.content_type == "application/vnd.debian.binary-package")')
 echo "BINARY_PACKGE_ASSET: $BINARY_PACKGE_ASSET"
 
 ASSET_NAME=$(echo $BINARY_PACKGE_ASSET | jq -r '.name')
@@ -18,8 +45,11 @@ FILE_NAME=meshtasticd_arm64.deb
 
 curl -L -o $FILE_NAME $ASSET_URL
 
-apt list -a libyaml-cpp-dev
-apt list -a libulfius-dev
+# list versions of libyaml-cpp-dev
+apt madison libyaml-cpp-dev
+
+# list versions of libulfius-dev
+apt madison libulfius-dev
 
 apt install -y -f libyaml-cpp-dev=0.7.0+dfsg-8
 apt install -y -f libulfius-dev=2.7
